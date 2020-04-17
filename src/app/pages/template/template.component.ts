70be664cc0fea8c6014758cea0772eba2441783c
Jesus Aranda <aranda.jm@icloud.com>
2020-04-17 12:35:37 -0300

    Creating pages for the forms and addind routing to the project

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+})
+export class TemplateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
