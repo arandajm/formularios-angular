835caadd08bf834d88e3fc66f9911ac22267ed56
Jesus Aranda <aranda.jm@icloud.com>
2020-04-18 01:22:55 -0300

    Creating formGroup for reactive form and define the fields

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
 })
 export class ReactiveComponent implements OnInit {
-  constructor() {}
+  // Declare Form
+  reactiveForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit(): void {}
+
+  createForm() {
+    this.reactiveForm = this.fb.group({
+      // Define form field
+      nombre: [''],
+      apellido: [''],
+      email: [''],
+    });
+  }
+
+  guardar() {
+    console.log(this.reactiveForm.value);
+  }
 }
